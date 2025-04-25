10f3d67ce48bbaeb2ecc2e7b236262402d4a210a
Abishek0411 <abishekram0411@gmail.com>
2025-04-25 18:30:58 +0530

    onrender deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "Bajaj-Doctor-Listing-Page",  // 👈 Add this
   server: {
     host: "::",
     port: 8080,
